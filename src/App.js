6036efa8719b7b582cf0cbf7e7a11fd41d615c5e
Annu <annury15@gmail.com>
2023-04-03 01:15:59 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const url = "url";
+  const [orderId, setOrderId] = useState("");
+  const [otp, setOtp] = useState("");
+
+  async function generateOtp() {
+    if (!orderId) {
+      return alert("enter order id");
+    }
+    const req = await fetch(`${url}/api/otp/generate/${orderId}`).catch(
+      (e) => e
+    );
+    const res = await req.json();
+    console.log(alert(res.message));
+  }
+
+  async function verifyOtp() {
+    if (!orderId) {
+      return alert("enter order id");
+    }
+    if (!otp?.length) {
+      return alert("enter otp");
+    }
+    const req = await fetch(`${url}/api/otp/verify/${orderId}`, {
+      method: "POST",
+      body: JSON.stringify({ otp }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).catch((e) => e);
+    const res = await req.json();
+    console.log(alert(res.message));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Authentication</h1>
+      <div className="otp-1">
+        Enter Order Id :
+        <input
+          className="order-id"
+          type="text"
+          placeholder="enter order id"
+          onChange={(e) => setOrderId(e.target.value)}
+        />
+        <button onClick={generateOtp} className="btn-o">
+          Generate otp
+        </button>
+      </div>
+      <div className="otp-2">
+        Enter Otp :
+        <input
+          className="otp-enter"
+          type="text"
+          placeholder="enter otp"
+          onChange={(e) => setOtp(e.target.value)}
+        />
+      </div>
+      <button className="btn" onClick={verifyOtp}>
+        Submit
+      </button>
     </div>
   );
 }
